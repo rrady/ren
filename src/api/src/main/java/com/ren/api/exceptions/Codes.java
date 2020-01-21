494cceb97fe86abe2393f23c371a515abc1fb55b
Andrei-Cosmin NEAGU <aneagu@deloittece.com>
2020-01-21 12:45:37 +0200

    exception handling for @Valid constraints

--- src/api/src/main/java/com/ren/api/exceptions/Codes.java
+++ src/api/src/main/java/com/ren/api/exceptions/Codes.java
@@ -5,6 +5,7 @@ public final class Codes {
     public static final String RESOURCE_ALREADY_EXISTS = "resource_already_exists";
     public static final String RESOURCE_EXPIRED = "resource_expired";
     public static final String INVALID_INPUT = "invalid_input";
+    public static final String INVALID_RESOURCE = "invalid_resource";
 
     private Codes() {
     }
--- src/api/src/main/java/com/ren/api/exceptions/RenExceptionHandler.java
+++ src/api/src/main/java/com/ren/api/exceptions/RenExceptionHandler.java
@@ -1,18 +1,26 @@
 package com.ren.api.exceptions;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-//@RestControllerAdvice
+@ControllerAdvice
 public class RenExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(RenException.class)
-	public final ResponseEntity<RenResource> handleException(RenException ex, WebRequest request) {
+	public final ResponseEntity<RenResource> handleException(RenException ex) {
 		RenResource exceptionResponse = new RenResource(ex.getCode(), ex.getMessage());
-		return new ResponseEntity<RenResource>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		RenResource exceptionResource = new RenResource(Codes.INVALID_RESOURCE, ex.getMessage());
+		return new ResponseEntity<>(exceptionResource, status);
 	}
 }
